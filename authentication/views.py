081684858542d2340b836cb5662457f7fa7fe413
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:12:24 +0300

    Passenger signup view linked up with the abstract user prooperty - is_ passenger

--- authentication/views.py
+++ authentication/views.py
@@ -13,3 +13,17 @@ def driver_signup(request):
     return render(request, 'registration/driver_signup.html', {'form': form})
 
 
+def passenger_signup(request):
+    if request.method == 'POST':
+        form = PassengerSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.is_passenger = True
+            user.is_driver = False
+            user.save()
+            return redirect(create_passenger_profile)
+    else:
+        form = PassengerSignupForm()
+    return render(request, 'registration/passenger_signup.html', {'form': form})
+

4a7f07898d1fa5204b4dbcdc62b37323dd99a646
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:08:49 +0300

    form for creating the passenger profile  created

---
+++ passenger/forms.py
@@ -0,0 +1,9 @@
+from passenger.models import PassengerProfile, pass_location
+from django import forms
+
+
+class CreatePassengerProfileForm(forms.ModelForm):
+    class Meta:
+        model = PassengerProfile
+        exclude = ['user']
+        exclude = ['user']

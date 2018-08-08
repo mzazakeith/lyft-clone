172f27a833beb68ad6c1a4952e399ebdd99841fb
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:44:35 +0300

    urls for the authentication app addded

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,2 +1,14 @@
-    url(r'^',include('driver.urls')),    url(r'^',include('driver.urls')),
+from django.conf.urls import url, include
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^signup/driver/$', views.driver_signup, name='signup-driver'),
+    url(r'^signup/passenger/$', views.passenger_signup, name='signup-passenger'),
+    url(r'^',include('driver.urls')),
     url(r'^', include('passenger.urls')),
+    url(r'^$', views.landing, name='land'),
+    url(r'^home', views.home, name='home'),
+    url(r'^userprofile/(?P<user_id>\d+)', views.userprofile, name='profile'),
+]

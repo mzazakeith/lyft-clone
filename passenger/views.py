bf42aa390379d91a4ef494e3d2711ab6063e8ec8
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:09:55 +0300

    When a form is created a default location is saved "Garden of Eden--lol"

--- passenger/views.py
+++ passenger/views.py
@@ -7,3 +7,8 @@ def create_passenger_profile(request):
             profile = form.save(commit=False)
             profile.user = current_user
             profile.save()
+            pass_location(user=current_user,pickup_location="Garden Of Eden").save()
+            return redirect('/')
+    else:
+        form = CreatePassengerProfileForm()
+    return render(request, 'profile/passenger.html', {"form": form , "user":current_user})

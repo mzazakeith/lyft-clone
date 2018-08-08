ca1ab6455255443bbd5b2151190b197b71732492
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:44:06 +0300

    trying to add a maps using geojson and leaflets fingers crossed

--- driver/models.py
+++ driver/models.py
@@ -1,9 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+from pyuploadcare.dj.models import ImageField
+from django.contrib.auth import get_user_model
+from djgeojson.fields import PointField
 
 User = get_user_model()
 
 Gender_Choices = (
     ('F', 'female'),
     ('M', 'male'),
+)
+
+Ratings = (
+    ('1', 'Poor'),
+    ('2', 'Below-Average'),
+    ('3', 'Average'),
+    ('4', 'Good'),
+    ('5', 'Excellent'),
 )
 
 
@@ -43,3 +56,10 @@ class driver_location(models.Model):
     def update_locationr(location):
         location.update()
 
+class Points(models.Model):
+    geom = PointField()
+
+
+class DriverReview(models.Model):
+    rate = models.CharField(max_length=30, choices=Ratings, default='None', blank=True)
+    user = models.ForeignKey(User)

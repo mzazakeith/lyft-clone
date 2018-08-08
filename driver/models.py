bcebc2a62da4d8365bc461f2e3499fefa0970e37
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:29:47 +0300

    driver profile all its class methods gender choices for the class added

--- driver/models.py
+++ driver/models.py
@@ -1,4 +1,28 @@
 
+User = get_user_model()
+
+Gender_Choices = (
+    ('F', 'female'),
+    ('M', 'male'),
+)
+
+
+class DriverProfile(models.Model):
+    profile_pic = ImageField()
+    gender = models.CharField(max_length=30, choices=Gender_Choices, default='None', blank=True)
+    phone_number = models.PositiveIntegerField()
+    user = models.ForeignKey(User)
+
+
+    @classmethod
+    def delete_driver(id, self):
+        self.delete()
+
+    @classmethod
+    def update_driver(driver):
+        driver.update()
+
+
 class Car(models.Model):
     car_capacity = models.PositiveIntegerField(default=3)
     car_number_plate = models.CharField(max_length=255)

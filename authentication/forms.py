4bda4b7367938b52c109a73358803c925cae8dd9
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:04:38 +0300

    Authenitcation forms done : driver andpassenger can sign up and there roles defined using an abstract user

---
+++ authentication/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class DriverSignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class PassengerSignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+

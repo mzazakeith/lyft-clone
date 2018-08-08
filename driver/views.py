0510678aec6c7f26b41d702bbe3611ca34af496f
mzazakeith <mzazakeith@gmail.com>
2018-08-08 19:15:46 +0300

    view for creating a drivers profile on signup

---
+++ driver/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from driver.forms import CreateDriverProfileForm, SubmitCarForm
+from driver.models import driver_location
+
+
+@login_required
+def create_driver_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = CreateDriverProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+            driver_location(user=current_user, current_location="None", destination="None").save()
+            return redirect(submit_car)
+    else:
+        form = CreateDriverProfileForm()
+    return render(request, 'profile/driver.html', {"form": form, "user": current_user})
+
+
